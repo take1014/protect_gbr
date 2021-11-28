a6be7a09710441b211334525edd105f563e236a2
take1014 <y.takehara1014@gmail.com>
2021-11-28 16:35:28 +0900

    show image shape, bbox image

--- analyzer.py
+++ analyzer.py
@@ -15,26 +15,7 @@ def draw_bounding_box(img, loc_list):
         img = cv2.line(img, (loc['x'], loc['y']+loc['height']), (loc['x']+loc['width'], loc['y']+loc['height']), (0, 0, 255), 2)
     return img
 
-def calc_bbox_rate(df_anno):
-    num_bbox = df_anno.apply(lambda x: len(ast.literal_eval(x)))
-    return (num_bbox>0).value_counts()/len(df)*100
-
-if __name__ == '__main__':
-    dataset_dir = "/home/take/fun/dataset/kaggle/tensorflow-great-barrier-reef"
-
-    # read csv
-    df = pd.read_csv(os.path.join(dataset_dir, 'train.csv'))
-
-    # calc bounding box rate
-    data = calc_bbox_rate(df['annotations'])
-    print(f"No BBox: {data[0]:0.2f}% | With BBox: {data[1]:0.2f}%")
-
-    # get annotation's list
-    anno_list = get_annotation_list(dataset_dir)
-
-    if not os.path.exists('./bd_img'):
-        os.mkdir('./bd_img')
-
+def output_bbox_img(anno_list):
     for i, anno in tqdm(enumerate(anno_list)):
         image_path = anno['img_path']
         loc_list   = anno['loc']
@@ -50,3 +31,32 @@ if __name__ == '__main__':
         draw_bounding_box(img, loc_list)
 
         cv2.imwrite(os.path.join(save_folder_path, '{}.jpg'.format(str(i).zfill(5))), img)
+
+if __name__ == '__main__':
+    dataset_dir = "/home/take/fun/dataset/kaggle/tensorflow-great-barrier-reef"
+
+    # read csv
+    df = pd.read_csv(os.path.join(dataset_dir, 'train.csv'))
+
+    # calc bounding box rate
+    count_bbox = df['annotations'].apply(lambda x: len(ast.literal_eval(x)))
+    data = (count_bbox>0).value_counts()/len(df)*100
+    print(f"False BBox: {data[0]:0.2f}% | True BBox: {data[1]:0.2f}%")
+
+    # get annotation's list
+    anno_list = get_annotation_list(dataset_dir)
+
+    if not os.path.exists('./bd_img'):
+        os.mkdir('./bd_img')
+
+    # get image shape and show bbox image
+    img =cv2.imread(anno_list[100]['img_path'])
+    print("image shape:(H,W,C):{}".format(img.shape))
+    draw_bounding_box(img, anno_list[100]['loc'])
+    cv2.imshow('test', img)
+    cv2.waitKey(10000)
+
+    # output all bounding box image.
+    # output_bbox_img(anno_list)
+
+
