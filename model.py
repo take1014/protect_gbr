3500744a07788be8640c849e42827b7d660866fc
take1014 <y.takehara1014@gmail.com>
2021-11-28 14:34:16 +0900

    add model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+import torch
+import torch.nn as nn
+
+
+class PGBRModel(nn.Module):
+    def __init__(self):
+        super(PGBRModel, self).__init__()
+
+    def forward(self, x):
+        return x
+
+if __name__ == "__main__":
+    model = PGBRModel()
+    print(model)
